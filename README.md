11bae406106157bdcca5fd102ae00bff8eaeb9f8
spaceface <spaceface7777@gmail.com>
2021-03-27 15:04:36 +0100

    minor tweaks

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 a script that calculates the minimum number of link clicks required to get from one Wikipedia entry to another
 
-NOTE: requires a dump of the Wikipedia database, which you can either [download from the releases page](https://github.com/spaceface777/WikiGameSolver/releases) (en only), or [generate yourself](db_gen/)
+NOTE: requires an adapted dump of the Wikipedia database, saved as the local file `db.sqlite` (roughly 7GB). You can [download one from the releases page](https://github.com/spaceface777/WikiGameSolver/releases) (English articles only), or [generate one yourself](db_gen/).
--- client.v
+++ client.v
@@ -14,29 +14,32 @@ fn main() {
 	mut app := &App{
 		db: sqlite.connect('db.sqlite') or { panic(err) }
 	}
-	os.input('') // not sure why this is needed, but it prevents an issue
-	mut start_time := time.new_stopwatch({})
-	for {
-		start := os.input('\nenter a starting Wikipedia entry: ')
-		end := os.input('enter a final Wikipedia entry: ')
-		start_time.restart()
-		path := app.find_path(start, end) or {
-			eprintln(term.red('error: $err'))
-			continue
-		}
-		println('found a path:')
-		for i, node in path {
-			println(' ${i + 1}. $node')
-		}
-		elapsed := f64(start_time.elapsed().milliseconds()) / 1000
-		println('took ${elapsed:0.2} seconds')
+	mut start, mut target := if os.args.len == 1 {
+		os.input('\nenter a starting Wikipedia entry: '),
+		os.input('enter a target Wikipedia entry: ')
+	} else {
+		os.args[1],
+		os.args[2]
 	}
+	mut start_time := time.new_stopwatch({})
+	path := app.find_path(start, target) or {
+		eprintln(term.red('error: $err'))
+		return
+	}
+	println('found a path:')
+	for i, node in path {
+		println(' ${i + 1}. $node')
+	}
+	elapsed := f64(start_time.elapsed().milliseconds()) / 1000
+	println('\ntook ${elapsed:.2f} seconds')
 }
 
 fn (mut app App) get(db_entry string) []string {
 	// clean_entry := db_entry.replace_each()
 	res := app.db.exec_one('SELECT links FROM data WHERE name = "$db_entry"') or { return [] }
-	return res.vals[0].split('\x01')
+	first := res.vals[0]
+	split := first.split('\x01')
+	return split
 }
 
 // this is an optimized iddfs / bfs hybrid,
@@ -47,10 +50,11 @@ fn (mut app App) find_path(start string, target string) ?[]string {
 		return error('start page `$start` not in the database')
 	}
 	if app.get(target).len == 0 {
-		return error('end page `$start` not in the database')
+		return error('target page `$target` not in the database')
 	}
 	for depth := 1; ; depth++ {
-		if path := app.dfs(start, target, 0, depth, []) {
+		path := app.dfs(start, target, 0, depth, [])
+		if path.len > 0 {
 			return path
 		}
 	}
@@ -62,7 +66,7 @@ fn (mut app App) find_path(start string, target string) ?[]string {
 // TODO: threading
 // TODO: make this non-recursive?
 // i guess C compilers might add a TCO optimization in `-prod` mode, so that may be unnecessary
-fn (mut app App) dfs(node string, target string, depth int, limit int, path_ []string) ?[]string {
+fn (mut app App) dfs(node string, target string, depth int, limit int, path_ []string) []string {
 	if node == target {
 		mut path := path_.clone()
 		path << node
@@ -71,11 +75,11 @@ fn (mut app App) dfs(node string, target string, depth int, limit int, path_ []s
 
 	if depth > limit {
 		app.depths[node] = depth
-		return none
+		return []
 	}
 
 	if app.depths[node] >= limit - depth {
-		return none
+		return []
 	}
 
 	if limit > depth + 1 {
@@ -83,12 +87,13 @@ fn (mut app App) dfs(node string, target string, depth int, limit int, path_ []s
 		mut path := path_.clone()
 		path << node
 		for child in children {
-			if found := app.dfs(child, target, depth + 1, limit, path) {
-				return found
+			res := app.dfs(child, target, depth + 1, limit, path)
+			if res.len > 0 {
+				return res
 			}
 		}
 	}
 
 	app.depths[node] = limit - depth
-	return none
+	return []
 }
