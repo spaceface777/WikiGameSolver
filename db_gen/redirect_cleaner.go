d5c5b433eb1fbe1c809f484fd35573eb3bb871a0
spaceface <spaceface7777@gmail.com>
2021-08-25 09:05:15 +0200

    redirect_cleaner: also handle multiple langs

--- db_gen/redirect_cleaner.go
+++ db_gen/redirect_cleaner.go
@@ -3,15 +3,15 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"os"
 	"strings"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 const (
-	file     = "./dump.xml"
-	db_file  = "./db.sqlite"
-	tdb_file = "file:tdb.sqlite?mode=ro"
+	db_file  = "./[LANG].db"
+	tdb_file = "file:TEMP_[LANG].db?mode=ro"
 )
 
 func uniques(s []string) []string {
@@ -34,61 +34,63 @@ func uniques(s []string) []string {
 }
 
 func main() {
-	db, _ := sql.Open("sqlite3", db_file)
-	tdb, _ := sql.Open("sqlite3", tdb_file)
+	for _, lang := os.Args[1..] {
+		db, _ := sql.Open("sqlite3", strings.Replace(db_file, "[LANG]", lang))
+		tdb, _ := sql.Open("sqlite3", strings.Replace(tdb_file, "[LANG]", lang))
 
-	fmt.Println("caching db into memory...")
-	db.Exec("CREATE TABLE IF NOT EXISTS data (name TEXT PRIMARY KEY, links TEXT)")
-	// gotta go fast
-	db.Exec("PRAGMA locking_mode = EXCLUSIVE")
-	db.Exec("BEGIN TRANSACTION")
-	ins, _ := db.Prepare("INSERT OR REPLACE INTO data VALUES (?, ?)")
+		fmt.Println("caching db into memory...")
+		db.Exec("CREATE TABLE IF NOT EXISTS data (name TEXT PRIMARY KEY, links TEXT)")
+		// gotta go fast
+		db.Exec("PRAGMA locking_mode = EXCLUSIVE")
+		db.Exec("BEGIN TRANSACTION")
+		ins, _ := db.Prepare("INSERT OR REPLACE INTO data VALUES (?, ?)")
 
-	var row_count int
-	if x := tdb.QueryRow("SELECT COUNT(*) FROM data"); x != nil {
-		x.Scan(&row_count)
-	}
-
-	redirects := make(map[string]string)
-	redirect_entries, _ := tdb.Query("SELECT name, redirect FROM data WHERE redirect != \"\"")
-	for redirect_entries.Next() {
-		var k, v string
-		redirect_entries.Scan(&k, &v)
-		redirects[k] = v
-	}
-
-	x := float64(row_count - len(redirects))
-	fmt.Println("finished caching db into memory")
-
-	rows, _ := tdb.Query("SELECT * FROM data WHERE redirect = \"\"")
-	for i := 0; rows.Next(); i++ {
-		var name string
-		var links_ string
-		var redirect string
-		rows.Scan(&name, &links_, &redirect)
-		links := strings.Split(links_, "\x01")
-
-		if i%10000 == 0 {
-			db.Exec("END TRANSACTION")
-			db.Exec("BEGIN TRANSACTION")
-			fmt.Printf("\rprocessed %d rows (%.2f%%)", i, float64(i*100)/x)
+		var row_count int
+		if x := tdb.QueryRow("SELECT COUNT(*) FROM data"); x != nil {
+			x.Scan(&row_count)
 		}
 
-		for j, link := range links {
-			parts := strings.Split(link, "|")
-			if dest, found := redirects[parts[0]]; found {
-				parts[0] = dest
+		redirects := make(map[string]string)
+		redirect_entries, _ := tdb.Query("SELECT name, redirect FROM data WHERE redirect != \"\"")
+		for redirect_entries.Next() {
+			var k, v string
+			redirect_entries.Scan(&k, &v)
+			redirects[k] = v
+		}
+
+		x := float64(row_count - len(redirects))
+		fmt.Println("finished caching db into memory")
+
+		rows, _ := tdb.Query("SELECT * FROM data WHERE redirect = \"\"")
+		for i := 0; rows.Next(); i++ {
+			var name string
+			var links_ string
+			var redirect string
+			rows.Scan(&name, &links_, &redirect)
+			links := strings.Split(links_, "\x01")
+
+			if i%10000 == 0 {
+				db.Exec("END TRANSACTION")
+				db.Exec("BEGIN TRANSACTION")
+				fmt.Printf("\rprocessed %d rows (%.2f%%)", i, float64(i*100)/x)
 			}
-			links[j] = strings.Join(parts, "|")
-		}
-		ins.Exec(name, strings.Join(uniques(links), "\x01"))
-		// ins.Exec(name, strings.Join(links, "\x01"))
-	}
 
-	rows.Close()
-	ins.Close()
-	db.Exec("END TRANSACTION")
-	db.Exec("PRAGMA optimize")
-	db.Close()
-	tdb.Close()
+			for j, link := range links {
+				parts := strings.Split(link, "|")
+				if dest, found := redirects[parts[0]]; found {
+					parts[0] = dest
+				}
+				links[j] = strings.Join(parts, "|")
+			}
+			ins.Exec(name, strings.Join(uniques(links), "\x01"))
+			// ins.Exec(name, strings.Join(links, "\x01"))
+		}
+
+		rows.Close()
+		ins.Close()
+		db.Exec("END TRANSACTION")
+		db.Exec("PRAGMA optimize")
+		db.Close()
+		tdb.Close()
+	}
 }
